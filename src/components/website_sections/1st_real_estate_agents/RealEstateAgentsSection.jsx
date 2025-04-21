e468c905fc348e0fca83425b227db0fe57ebbfea
Ahmed Alamin <engahmedegypto@gmail.com>
2025-04-21 19:03:58 +0200

    change headeق

--- src/components/website_sections/1st_real_estate_agents/RealEstateAgentsSection.jsx
+++ src/components/website_sections/1st_real_estate_agents/RealEstateAgentsSection.jsx
@@ -1,42 +1,38 @@
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+
 import classes from "./RealEstateAgentsSection.module.css";
 
 import firstBg from "./images/first-background.jpg";
-import secondBg from "./images/2background.jpg";
+import secondBg from "./images/3background.jpg";
 import thirdBg from "./images/3background.jpg";
 
 const backgroundImages = [firstBg, secondBg, thirdBg];
 
 export const RealEstateAgentsSection = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prev) => (prev + 1) % backgroundImages.length);
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className={classes.sectionWrapper}>
-      {/* Background image with smooth transition */}
-      <motion.div
-        className={classes.background}
-        key={currentImageIndex}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        style={{ backgroundImage: `url(${backgroundImages[currentImageIndex]})` }}
-      />
+      <Swiper
+        modules={[Autoplay, EffectFade]}
+        effect="fade"
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        loop={true}
+        className={classes.swiperContainer}
+      >
+        {backgroundImages.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className={classes.slide}
+              style={{ backgroundImage: `url(${image})` }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div className={classes.overlay}>
-        <motion.div
-          className={classes.textBlock}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.5, duration: 1 }}
-        >
+        <div className={classes.textBlock}>
           <h1 className={classes.titleBig}>
             <span className={classes.orangeTitleText2}>
               من اختيـارك … الى استقرارك,
@@ -45,7 +41,7 @@ export const RealEstateAgentsSection = () => {
             دايـماً معـاك
           </h1>
           <p className={classes.littleheader}>سهيل العقاريه ثقة تبنى مستقبلك</p>
-        </motion.div>
+        </div>
       </div>
     </div>
   );
