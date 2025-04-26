1d0b22397bee0c1b6c3d454dd63e602e79e5a5f4
Ahmed Alamin <engahmedegypto@gmail.com>
2025-04-27 01:09:08 +0300

    change headeق

--- src/components/website_sections/4th_portfolio_and_feedback/PortfolioAndFeedbackSection.jsx
+++ src/components/website_sections/4th_portfolio_and_feedback/PortfolioAndFeedbackSection.jsx
@@ -133,7 +133,7 @@ export const PortfolioAndFeedbackSection = forwardRef((props, ref) => {
           variants={itemVariants}
         >
           <motion.h2 className={classes.projectTitle} variants={itemVariants}>
-          سهيــل بوليفارد - حى القرينيه
+    سهيــل بوليفارد - حى القرينية
           </motion.h2>
           
           <Swiper
