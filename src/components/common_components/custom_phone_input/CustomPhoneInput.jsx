399e8d7402480f051d69ba1ab2d1b2fa2eea95af
Ahmed Alamin <engahmedegypto@gmail.com>
2025-05-13 15:57:25 +0300

    add images to protofolio

--- src/components/common_components/custom_phone_input/CustomPhoneInput.jsx
+++ src/components/common_components/custom_phone_input/CustomPhoneInput.jsx
@@ -1,43 +1,31 @@
 import { useState } from "react";
-import PhoneInput from "react-phone-input-2";
 import classes from "./CustomPhoneInput.module.css";
 
 export const CustomPhoneInput = ({ getValues, bgColor }) => {
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const phoneRegex = /^\+[\d]{1} \([\d]{2,3}\) [\d]{2,3}-[\d]{2,3}-[\d]{2}$/;
+  const handleInputChange = (e) => {
+    const value = e.target.value;
 
-    const setPhoneNumberToRuCode = () => {
-        setPhoneNumber("");
-        phoneNumberInputHandler("");
-    };
+    // اسمح فقط بالأرقام
+    const onlyDigits = value.replace(/\D/g, '');
 
-    const phoneNumberInputHandler = (event) => {
+    // تحقق من أن الرقم سعودي: يبدأ بـ 5 أو 05 وطوله من 9 إلى 10 أرقام
+    const isValid = /^(05\d{8}|5\d{8})$/.test(onlyDigits);
 
-        let inputNumber = String(event).replace(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/, "+$1 ($2) $3-$4-$5");
-        
-        if(inputNumber.match(phoneRegex)) {
-            getValues(inputNumber, true);
-        } else {
-            getValues(inputNumber, false);
-        }
+    setPhoneNumber(onlyDigits);
+    getValues(onlyDigits, isValid);
+  };
 
-        setPhoneNumber(inputNumber);
-
-    };
-
-
-    return (
-
-        <PhoneInput value={phoneNumber}
-            onChange={phoneNumberInputHandler} 
-            onFocus={setPhoneNumberToRuCode}
-            specialLabel={false} 
-            placeholder=" رقـم الهاتـف" 
-            
-            autoFormat={true}
-            inputClass={classes.phoneInput}
-            inputStyle={bgColor && {background: bgColor}}
-        />
-    );
-}
+  return (
+    <input
+      type="tel"
+      value={phoneNumber}
+      onChange={handleInputChange}
+      placeholder="رقم الهاتف السعودي (مثال: 05xxxxxxxx)"
+      className={classes.phoneInput}
+      style={bgColor ? { background: bgColor } : {}}
+      maxLength={10}
+    />
+  );
+};
